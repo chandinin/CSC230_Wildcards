1d10924f86de35e7d033d2bc89dfbe9066f39512
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-02 15:46:53 -0500

    Corrected errors in the create table sql statement

--- BidderTable.php
+++ BidderTable.php
@@ -11,30 +11,29 @@ $username = "wilddb_user";
 $password = "wilddb_db";
 $dbname = "wilddb";
 
-try{
-    $connection = new PDO("mysql:host = $servername;dbname = $dbname", $username, $password);
-
-    //PHP Data Objects exception
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //Create bidder table
     $sql = "CREATE TABLE BIDDER (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ID INT NOT NULL PRIMARY KEY,
 	BIDOPSID INT, 
-    FIRST NAME VARCHAR(30) NOT NULL,
-	LAST NAME VARCHAR(30),
+    FIRST_NAME VARCHAR(30) NOT NULL,
+	LAST_NAME VARCHAR(30),
     EMAIL VARCHAR(40) NOT NULL,
 	PASSWORD VARCHAR(255) NOT NULL,
-	PHONE INT, 
+	PHONE INT 
 );";
 
-    //use exec() because no results are returned
+    // use exec() because no results are returned
     $connection->exec($sql);
     echo "Table Bidder created successfully";
 }
-catch(PDOException){
+catch(PDOException $e)
+{
     echo $sql . "<br>" . $e->getMessage();
 }
-
 $connection = null;
 ?>
