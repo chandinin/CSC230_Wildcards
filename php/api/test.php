922b9a60a3a89fad5f534195552d69c628a994f6
Steven Mackey <stevester94@gmail.com>
2018-03-10 01:22:17 -0800

    Create initial shitty versions of bidder_registration screens

--- php/api/test.php
+++ php/api/test.php
@@ -1,1 +1,13 @@
-<?php echo phpinfo(); ?>
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+// Just send back what they've sent us as a JSON message
+print_r(json_encode($_POST));
+
+
+?>
+
