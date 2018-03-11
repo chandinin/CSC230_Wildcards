6e3b23592873f9b2ed897da8a840a5d623e9555b
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-11 04:10:06 -0400

    Fixed the js file

--- bidderlogin.js
+++ bidderlogin.js
@@ -6,15 +6,17 @@ function LoginAction() {
     else if (document.forms["loginForm"]["password"].value == "") {
         alert("Please enter your password ")
     }
-    var params = "UserName=" + document.forms["loginForm"]["username"].value +"chan&PASSWORD="+ document.forms["loginForm"]["password"].value;
+    var params = "UserName=" + document.forms["loginForm"]["username"].value + "chan&PASSWORD=" + document.forms["loginForm"]["password"].value;
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/bidder_authentication.php", true);
     xhttp.onreadystatechange = function () {
-        if(JSON.parse(xhttp.responseText).authenticated == true){
+        var response = JSON.parse(xhttp.responseText);
+        if (response.authenticated == true) {
             window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
+        }
+        else{
+            alert("Incorrect username or password ")
         }
     }
     xhttp.send(params);
-
-
 }
