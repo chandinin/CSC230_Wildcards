e31a06c520dfabd73191992534ae8aceec105d1b
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-12 00:35:29 -0400

    Added employee login.html and javascript

--- employeelogin.js
+++ employeelogin.js
@@ -1,22 +1,41 @@
-function LoginAction() {
-
-    if (document.forms["employeeLoginForm"]["username"].value == "") {
+function LoginAction(form) {
+    //Check to see if user left username or password field blank
+    if (form.username.value == "") {
         alert("Please enter user name")
     }
-    else if (document.forms["employeeLoginForm"]["password"].value == "") {
+    else if (form.password.value == "") {
         alert("Please enter your password ")
     }
-    var params = "UserName=" + document.forms["employeeLoginForm"]["username"].value + "chan&PASSWORD=" + document.forms["employeeLoginForm"]["password"].value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/bidder_authentication.php", true);
-    xhttp.onreadystatechange = function () {
-        var response = JSON.parse(xhttp.responseText);
-        if (response.authenticated == true) {
-            window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
+    else{
+        //read username and password from the HTML form
+        var params = {"UserName":form.username.value,
+            "PASSWORD":form.password.value};
+
+        //Convert HTML form value to json
+        var myJSON = JSON.stringify(params);
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/employee/authentication.php", true);
+        //Async call
+
+        xhttp.onreadystatechange  = function () {
+            var response = JSON.parse(xhttp.responseText);
+            var status = JSON.parse(xhttp.status);
+            if(status == 500){
+                alert("Internal Server error!")
+            }
+            if (status == 200 && response.authenticated == true) {
+                //IF the authentication successful go to the landing page, NOTE: Need to change to the correct URL
+                //window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
+                alert("Login Successful!!")
+            }
+            else{
+                alert("Incorrect username or password ")
+            }
         }
-        else{
-            alert("Incorrect username or password ")
-        }
+        alert(myJSON);
+        //Send POST request to server
+        xhttp.send(myJSON);
     }
-    xhttp.send(params);
 }
