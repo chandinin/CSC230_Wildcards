677676bbe20fd626abc788d66e3b5c67436ad38e
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-12 00:24:33 -0400

    need to fix known bug: if I comment the alert the page doesnt work, it works only when i leave the alert message uncommented

--- bidderlogin.js
+++ bidderlogin.js
@@ -1,5 +1,5 @@
 function LoginAction(form) {
-
+    //Check to see if user left username or password field blank
     if (form.username.value == "") {
         alert("Please enter user name")
     }
@@ -7,27 +7,35 @@ function LoginAction(form) {
         alert("Please enter your password ")
     }
     else{
-
+        //read username and password from the HTML form
         var params = {"UserName":form.username.value,
                     "PASSWORD":form.password.value};
+
+        //Convert HTML form value to json
         var myJSON = JSON.stringify(params);
-        alert(myJSON);
+
         var xhttp = new XMLHttpRequest();
+
         xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/bidder/authentication.php", true);
+        //Async call
 
         xhttp.onreadystatechange  = function () {
             var response = JSON.parse(xhttp.responseText);
-            if(JSON.parse(xhttp.status) == 500){
+            var status = JSON.parse(xhttp.status);
+            if(status == 500){
                 alert("Internal Server error!")
             }
-            if (response.authenticated == true) {
-
-                window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
+            if (status == 200 && response.authenticated == true) {
+                //IF the authentication successful go to the landing page, NOTE: Need to change to the correct URL
+                //window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
+                alert("Login Successful!!")
             }
             else{
                 alert("Incorrect username or password ")
             }
         }
+        alert(myJSON);
+        //Send POST request to server
         xhttp.send(myJSON);
     }
 }
