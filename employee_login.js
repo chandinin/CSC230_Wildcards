cdc5c2b8745c8eee979dc93d23002df0489454eb
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-15 14:18:09 -0400

    Added home page link to opportunity and register screens

--- employee_login.js
+++ employee_login.js
@@ -1,28 +1,32 @@
-function EmployeeLoginAction(form) {
+function EmployeeLoginAction() {
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     //Check to see if user left username or password field blank
-    if (form.username.value == "") {
+    if (username == "") {
         alert("Please enter user name")
     }
-    else if (form.password.value == "") {
+    else if (password == "") {
         alert("Please enter your password ")
     }
     else{
         //read username and password from the HTML form
-        var params = {"UserName":form.username.value,
-            "PASSWORD":form.password.value};
+        var params = {"UserName":username,
+            "PASSWORD":password};
 
         //Convert HTML form value to json
         var myJSON = JSON.stringify(params);
         var xhttp = new XMLHttpRequest();
 
-        xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/employee/authentication.php", false);
+        xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/employee/authentication.php", true);
         //Async call
 
         xhttp.onload  = function () {
-            if (this.status == 200 && this.response.authenticated == true) {
+            var response = JSON.parse(xhttp.responseText);
+            var status = JSON.parse(xhttp.status);
+            if (status == 200 && response.authenticated == true) {
                 //IF the authentication successful go to the landing page, NOTE: Need to change to the correct URL
-                window.location.replace("http://athena.ecs.csus.edu/~mackeys/bidder_register.html")
-                alert("Login Successful!!")
+                window.location.replace("Opportunity.html")
             }
             else{
                 alert("Incorrect username or password ")
