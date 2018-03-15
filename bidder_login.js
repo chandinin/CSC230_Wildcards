bfe469806efe0377f959f9394a5f633850915125
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-15 14:27:14 -0400

    Fixed the lik for bidder landing

--- bidder_login.js
+++ bidder_login.js
@@ -1,29 +1,30 @@
-function BidderLoginAction(form) {
+function BidderLoginAction() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     //Check to see if user left username or password field blank
-    if (form.username.value == "") {
+    if (username == "") {
         alert("Please enter user name")
     }
-    else if (form.password.value == "") {
+    else if (password == "") {
         alert("Please enter your password ")
     }
     else{
         //read username and password from the HTML form
-        var params = {"UserName":form.username.value,
-                    "PASSWORD":form.password.value};
+        var params = {"UserName":username,
+                    "PASSWORD":password};
 
         //Convert HTML form value to json
         var myJSON = JSON.stringify(params);
         var xhttp = new XMLHttpRequest();
 
-        xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/bidder/authentication.php", false);
+        xhttp.open("POST", "http://athena.ecs.csus.edu/~mackeys/php/api/bidder/authentication.php", true);
         //Async call
 
         xhttp.onload = function () {
             var response = JSON.parse(xhttp.responseText);
             var status = JSON.parse(xhttp.status);
             if (status == 200 && response.authenticated == true) {
-               //window.location.replace("https://www.google.com")
-                alert("Login Successful!!")
+                window.location.replace("bidder_landing.html")
             }
             else{
                 alert("Incorrect username or password ")
