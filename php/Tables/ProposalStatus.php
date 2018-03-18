80a1ede5f848281b7e59669f60c8c01f25e2a8f8
Chandini Nagendra <chandini.nagendra@mobeewave.com>
2018-03-18 03:44:06 -0400

    fixed Proposal status table

--- php/Tables/ProposalStatus.php
+++ php/Tables/ProposalStatus.php
@@ -21,7 +21,7 @@ try {
 	[StatusId] INT NOT NULL,
 	[Description] varchar(50) NOT NULL,
 	CONSTRAINT [PK_ProposalStatus] PRIMARY KEY ([StatusId]),
-	CONSTRAINT [FK_ProposalStatus_StatusId] FOREIGN KEY ([StatusId]) REFERENCES Proposal ([status])
+	CONSTRAINT [FK_ProposalStatus_StatusId] FOREIGN KEY ([StatusId]) REFERENCES Proposal ([Status])
 );";
     // use exec() because no results are returned
     $connection->exec($sql);
