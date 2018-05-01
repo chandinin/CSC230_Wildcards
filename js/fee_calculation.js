537e77d910f6c993d37f57980d73e6b5faff014b
chandinin <nagendra.chandini@gmail.com>
2018-05-01 05:22:11 -0400

    fixed all broken screens

--- js/fee_calculation.js
+++ js/fee_calculation.js
@@ -1,3 +1,10 @@
+var proposalID = localStorage.getItem("proposalID");
+
+$(document).ready(
+    function () {
+        document.getElementById("proposal").innerHTML = proposalID;
+        getBidderDetails();
+    });
 function calculateFee(){
     var lowestFeeProposal = document.getElementById("lowestFee").value;
     var proposerFee = document.getElementById("feeProposal").value;
@@ -17,7 +24,7 @@ function calculateFee(){
 }
 
 function updateProposalTotalScore(feeProposalScore){
-    var updateProposalScore = {"ProposalID":1,"FinalTotalScore":feeProposalScore};
+    var updateProposalScore = {"ProposalID":proposalID,"FinalTotalScore":feeProposalScore};
 
     var updateProposal = JSON.stringify(updateProposalScore);
     var xhttp = new XMLHttpRequest();
@@ -30,4 +37,20 @@ function updateProposalTotalScore(feeProposalScore){
         }
     }
     xhttp.send(updateProposal);
+}
+
+//get bidder details based on proposal id
+function getBidderDetails() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','http://athena.ecs.csus.edu/~wildcard/php/api/proposal/getBidder.php?ProposalID='+proposalID,true);
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            var bidderDetails = JSON.parse(xhr.responseText);
+            bidderName = bidderDetails.first_name +" " + bidderDetails.last_name;
+            document.getElementById("bidderName").innerHTML = bidderName;
+        } else {
+            alert("Error response");
+        }
+    };
+    xhr.send();
 }
