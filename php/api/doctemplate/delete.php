408b62bc520168969ae1926124a94c92c924d8ae
danejew625 <dane_jew@hotmail.com>
2018-05-08 19:10:53 -0700

    Fix for doctemplate / delete to accept the DocTemplateID.

--- php/api/doctemplate/delete.php
+++ php/api/doctemplate/delete.php
@@ -19,16 +19,15 @@ $_POST_LowerCase = array_change_key_case($_POST, CASE_LOWER);
 if(isSet($_POST_LowerCase["doctemplateid"]))
 {
   $doctemplateID = $_POST_LowerCase["doctemplateid"];
-  $DocTemplateIDArr = explode("_", $doctemplateID);
 
   //Search
   $doctemplate->DocTemplateID = $doctemplateID;
-  $doctemplate->selectByDocTemplateID($DocTemplateIDArr[1]);
+  $doctemplate->selectByDocTemplateID($doctemplateID);
 
   //delete doctemplate.
   if($doctemplate->delete())
   {  
-    $doctemplate->deleteOppDocTemplateByID($DocTemplateIDArr[1]);
+    $doctemplate->deleteOppDocTemplateByID($doctemplateID);
 
     echo '{';
     echo ' message : "Delete suceeded.  (DoctemplateID='.$doctemplateID.')"';
@@ -42,3 +41,4 @@ if(isSet($_POST_LowerCase["doctemplateid"]))
   }
 }
 ?>
+
