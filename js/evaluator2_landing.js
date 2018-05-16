29cbc816ff96d56f393e4392ad0d80e399e27a00
chandinin <nagendra.chandini@gmail.com>
2018-05-16 02:55:09 -0400

    Commit changes

--- js/evaluator2_landing.js
+++ js/evaluator2_landing.js
@@ -13,12 +13,11 @@ $(document).ready(
         });
     });
 
-//Get all opportunity list from server
+//Get all opportunity list from server where eval 1 is complete
 function getOppList() {
     $('#oppListTableBody').empty();
     var xhr = new XMLHttpRequest();
-    xhr.open('GET','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/read.php',true);
-    //xhr.open('GET','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/read.php?Status=4',true);
+    xhr.open('GET','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/read.php?Status=4',true);
     xhr.onload = function() {
         if (xhr.status == 200) {
             var jsonArray = JSON.parse(xhr.responseText);
